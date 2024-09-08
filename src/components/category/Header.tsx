3d37d97486d365c1d35a8a9ee61f8d00affd3b31
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-08 17:54:07 +0600

    style: text overflow issue in category page

--- src/components/category/Header.tsx
+++ src/components/category/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header text-center my-8">
       <PageHeading>Workspace</PageHeading>
-      <p className="text-sm text-muted-foreground mx-auto w-[30rem]">
+      <p className="text-sm text-muted-foreground mx-auto px-4 md:w-[30rem]">
         The secret to a tidy desk? Don&apos;t get rid of anything, just put it in
         really really nice looking containers.
       </p>
