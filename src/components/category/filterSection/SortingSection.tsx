ea79a370dee4f8b53da40d55fd3607553c4a4760
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-09-14 21:54:50 +0600

    feat: adding sorting dropdown

--- src/components/category/filterSection/SortingSection.tsx
+++ src/components/category/filterSection/SortingSection.tsx
@@ -1,18 +1,41 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 function SortingSection() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <>
       <div className="right">
-        <Button
-          variant={"text"}
-          className="text-muted-foreground flex items-center text-xs"
-        >
-          <span>Sort</span>
-          <ChevronDown className="size-3 ml-2" />
-        </Button>
+        <DropdownMenu open={showMenu} onOpenChange={setShowMenu}>
+          <DropdownMenuTrigger>
+            <Button
+              variant={"text"}
+              className={`text-muted-foreground flex items-center text-xs ${showMenu ? "text-primary" : ''}`}
+              onClick={() => setShowMenu(true)}
+            >
+              <span>Sort</span>
+              <ChevronDown className={`size-3 ml-2 ${showMenu ? "" : "-rotate-180"} transition-all duration-300`} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="text-xs">Newest</DropdownMenuItem>
+              <DropdownMenuItem className="text-xs">Price: Lower to Highest</DropdownMenuItem>
+              <DropdownMenuItem className="text-xs">Price: Highest to Lower</DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </>
   );
